5313935966c26e64b6080d0d167ee008ff975715
Johnnatan Sanchez <jonpool94@gmail.com>
2018-04-22 01:17:04 -0400

    Esto no va aqui

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import firebase from 'firebase';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
-firebase.initializeApp({
-	apiKey: "AIzaSyDX2DocNLOjR66u7Jgx3Kswky0LtXN2kXI",
-    authDomain: "custepappids.firebaseapp.com",
-    databaseURL: "https://custepappids.firebaseio.com",
-    projectId: "custepappids",
-    storageBucket: "custepappids.appspot.com",
-    messagingSenderId: "543699124252"
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
